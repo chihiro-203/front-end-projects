0f793f3e0c30cd2c8ad793a735ba1a80e29a574e
ume <chi.nguyenthiyen.203@gmail.com>
2025-03-15 21:27:25 +0700

    Change theme of form of login/sign

--- 8-login-signup/script.js
+++ 8-login-signup/script.js
@@ -2,10 +2,28 @@ const container = document.getElementById("form-container");
 const registerBtn = document.getElementById("signup");
 const loginBtn = document.getElementById("login");
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
 });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
 });
+
+const pwHids = document.querySelectorAll(".pw-hid");
+pwHids.forEach((pwHid) => {
+  pwHid.addEventListener("click", () => {
+    const passwordInput = pwHid.closest(".box").querySelector("input");
+
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      pwHid.classList.remove("uil-eye-slash");
+      pwHid.classList.add("uil-eye");
+    } else {
+      passwordInput.type = "password";
+      pwHid.classList.remove("uil-eye");
+      pwHid.classList.add("uil-eye-slash");
+    }
+  });
+});
+
