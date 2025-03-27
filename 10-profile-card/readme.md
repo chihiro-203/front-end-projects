c9e4f3440a1434a858dba3cb434a6b330be2fd80
ume <chi.nguyenthiyen.203@gmail.com>
2025-03-28 01:40:40 +0700

    Finish project 10th - Profile Card

---
+++ 10-profile-card/readme.md
@@ -0,0 +1,17 @@
+[Return to README.md](https://github.com/nguyenthiyenchi/front-end-projects/blob/main/README.md)
+
+## Profile Card
+1. **Project Demo:**
+
+![Profile Card](./resources/demo.gif)
+
+2. **Description:** 
+    - About Me – A short introduction to provide users with a glimpse of who I am.
+    - My Projects – A section highlighting my projects, including their names and links to GitHub for easy access.
+    - Contact Form – A form for users to get in touch, providing a space for sending messages directly to me.
+    
+3. **Purpose:** 
+    - Create an interactive and simple profile card that can be used to showcase my personal details, projects, and allow others to contact me.
+    - Present my work and make it easy for potential collaborators or interested parties to reach out.
+
+4. **Link Demo**: [simple-calculator](https://chihiro-203.github.io/front-end-projects/10-prodile-card/)
--- 10-profile-card/script.js
+++ 10-profile-card/script.js
@@ -8,10 +8,58 @@ lists.forEach((item, i) => {
     item.classList.add("active");
     boxes[i].classList.add("show");
   });
-
 });
 
 function closeAllBoxes() {
   boxes.forEach((box) => box.classList.remove("show"));
   lists.forEach((list) => list.classList.remove("active"));
 }
+
+
+// Contact form
+const form = document.getElementById("c-contact-form");
+const alert = document.querySelector(".contact-form-alert");
+
+// Initialize EmailJS
+(function () {
+  emailjs.init({
+    publicKey: "wQP9ZyJKxe0nRtCvx",
+  });
+  console.log("EmailJS initialized.");
+})();
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Input validation
+  const name = document.querySelector("[name='name']").value;
+  const email = document.querySelector("[name='email']").value;
+  const subject = document.querySelector("[name='subject']").value;
+  const message = document.querySelector("[name='message']").value;
+
+  if (!name || !email || !subject || !message) {
+    alert.innerHTML =
+      "<div class='error'><span>Please fill in all fields!</span> <i class='bx bxs-error-alt'></i></div>";
+    return;
+  }
+
+  emailjs
+    .sendForm("service_wvizx8l", "template_ekmr813", "#c-contact-form")
+    .then(
+      () => {
+        console.log("Email sent successfully.");
+        alert.innerHTML =
+          "<div class='success'><span>Your message was sent successfully!</span> <i class='fa-solid fa-paw'></i></div>";
+        form.reset();
+        setTimeout(() => {
+          alert.innerHTML = "";
+        }, 5000);
+      },
+      (error) => {
+        console.error("Email sending failed:", error);
+        alert.innerHTML =
+          "<div class='error'><span>Your message was not sent!</span> <i class='bx bxs-error-alt'></i></div>";
+        alert.title = error.text;
+      }
+    );
+});
